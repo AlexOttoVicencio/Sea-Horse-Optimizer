4c87044e5f5ceebc408b2b2bc66992b439dba21e
AlexOttoVicencio <80718046+AlexOttoVicencio@users.noreply.github.com>
2023-09-14 13:36:37 -0300

    inicializacón

---
+++ SeaHorseAlgorithm.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#Sphere obj funtion parameters
+#falta escribir las sumatoria de la esfera
+LB=-100
+UB=100
+Dim=30
+
+#Algorithm parameters
+
+popsize=30;
+iterations=500;   
+ 
+
+#initializing array and values
