20a867e13ba8e4093493234837eb681f0905d068
AlexOttoVicencio <80718046+AlexOttoVicencio@users.noreply.github.com>
2023-10-04 18:53:54 -0300

    initialization

--- SeaHorseAlgorithm.py
+++ SeaHorseAlgorithm.py
@@ -1,15 +1,53 @@
 import numpy as np
+import random
 
 #Sphere obj funtion parameters
 #falta escribir las sumatoria de la esfera
 LB=-100
 UB=100
 Dim=30
-
-#Algorithm parameters
+Pop=10
 
 popsize=30;
-iterations=500;   
- 
+iterations=500   
+seahorses = np.zeros((Pop,Dim))
+print(seahorses)
+
+
+
+
+#--------------FUNCTIONS--------------------------------------
+
+
+#FIND ELITE SEAHORSE
+
+def elite_seahorse(seahorses):
+    print("los caballos")
+    print(seahorses)    
+
+
+
+
+
+
+
+
+
+#Fill with values between bounds
+with np.nditer(seahorses, op_flags=['readwrite']) as it:
+   i=1
+   for x in it:
+
+       x[...] = random.uniform(0,1) * (UB-LB) + LB
+#find elite seahores
+
+
+elite_seahorse(seahorses)
+
 
 #initializing array and values
+
+
+
+
+
